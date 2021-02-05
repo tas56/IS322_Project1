6d9bf9ce314c97d2deb8c6d1a7d3f25ac7b5e3d2
Thomas Semiz <tas56@njit.edu>
2021-02-04 23:43:55 -0500

    Created responsive/collapable navbar

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,6 +1,25 @@
-const toggleButton = document.getElementsByClassName('toggle-button')[0];
-const navbarLinks = document.getElementsByClassName('navbar-links')[0];
+
+// Collapsable header
+const toggleButton = document.querySelector('.toggle-button');
+const navbarLinks = document.querySelector('.navbar-links');
 
 toggleButton.addEventListener('click', ()=> {
     navbarLinks.classList.toggle('active');
 });
+
+// Carousel
+const carousel = document.getElementsByClassName('carousel_img');
+
+var currentSlide = 0;
+
+setInterval(() => {
+    if(currentSlide !== carousel.length-1){
+        carousel[currentSlide].classList.remove('active');
+        carousel[currentSlide + 1].classList.add('active');
+        currentSlide++;
+    } else {
+        carousel[currentSlide].classList.remove('active');
+        currentSlide = 0;
+        carousel[currentSlide].classList.add('active');
+    }
+},3000);
