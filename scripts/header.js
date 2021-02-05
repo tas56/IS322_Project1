c2d8c2aec444a7c6ff6b81cdf8d300cc00450186
Thomas Semiz <tas56@njit.edu>
2021-02-05 10:54:33 -0500

    updated products page

---
+++ scripts/header.js
@@ -0,0 +1,7 @@
+// Collapsable header
+const toggleButton = document.querySelector('.toggle-button');
+const navbarLinks = document.querySelector('.navbar-links');
+
+toggleButton.addEventListener('click', ()=> {
+    navbarLinks.classList.toggle('active');
+});
--- scripts/landing.js
+++ scripts/landing.js
@@ -1,16 +1,7 @@
-
-// Collapsable header
-const toggleButton = document.querySelector('.toggle-button');
-const navbarLinks = document.querySelector('.navbar-links');
-
-toggleButton.addEventListener('click', ()=> {
-    navbarLinks.classList.toggle('active');
-});
-
 // Carousel
 const carousel = document.getElementsByClassName('carousel_img');
 
-var currentSlide = 0;
+let currentSlide = 0;
 
 setInterval(() => {
     if(currentSlide !== carousel.length-1){
@@ -23,3 +14,4 @@ setInterval(() => {
         carousel[currentSlide].classList.add('active');
     }
 },3000);
+
